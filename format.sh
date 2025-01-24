7aa97be7a78a6c099f9dc62e1aeae76f1847397c
Kai Tinkess <kaitinkess@gmail.com>
2025-01-24 12:54:10 -0500

    fix format script

--- format.sh
+++ format.sh
@@ -3,5 +3,8 @@
 # Copy pre-commit hook if it doesn't exist
 cp -n pre-commit .git/hooks/pre-commit
 
+# Format repository
 find netlib -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -Werror -i
+find app -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -Werror -i
+
 echo "Formatting repository."

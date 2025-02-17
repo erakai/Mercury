9bf218dd0a48e02cb0bc548b358870860a5e6022
AlexHunton2 <alexhunton02@gmail.com>
2025-02-17 16:01:37 -0500

    HstpHandler final form (hopefully)

--- netlib/src/hstp.cpp
+++ netlib/src/hstp.cpp
@@ -1,29 +1,6 @@
 #include "hstp.hpp"
-#include "logger.hpp"
-#include <QtCore/qstringview.h>
-#include <cstddef>
-#include <cstdint>
-#include <cstring>
+#include <QtCore/qcontainerfwd.h>
 #include <memory>
-#include <string>
-#include <sys/_endian.h>
-
-bool HstpHandler::connect(QHostAddress &addr, int port)
-{
-  m_qtcp_sock->connectToHost(addr, port);
-
-  if (m_qtcp_sock->waitForConnected())
-  {
-    return true;
-  }
-
-  return false;
-}
-
-void HstpHandler::close()
-{
-  m_qtcp_sock->close();
-}
 
 bool HstpHandler::init_msg(char sender_alias[18])
 {
@@ -51,20 +28,26 @@ bool HstpHandler::add_option_echo(const char *msg)
 
   m_hdr->options.push_back(opt);
 
-  return false;
+  return true;
 }
 
-bool HstpHandler::send_msg()
+std::shared_ptr<QByteArray> HstpHandler::emit_msg()
 {
   if (get_status() != MSG_STATUS::IN_PROGRESS)
   {
-    log("Unable to send message, message not in progress", ll::ERROR);
-    return false;
+    log("Unable to emit message, uninitalized message.", ll::ERROR);
+    return nullptr;
   }
 
-  QByteArray byte_array = _serialize(*m_hdr);
+  std::shared_ptr<QByteArray> bytes = _serialize(m_hdr);
+  clear_msg();
+  return bytes;
+}
 
-  return true;
+std::shared_ptr<HSTP_Header>
+HstpHandler::bytes_to_msg(const std::shared_ptr<QByteArray> &buff)
+{
+  return _deserialize(buff);
 }
 
 void HstpHandler::clear_msg()
@@ -79,33 +62,36 @@ MSG_STATUS HstpHandler::get_status()
   return m_current_status;
 }
 
-QByteArray HstpHandler::_serialize(HSTP_Header &hdr)
+std::shared_ptr<QByteArray>
+HstpHandler::_serialize(const std::shared_ptr<HSTP_Header> &hdr)
 {
-  QByteArray byte_array;
-  QDataStream ds(&byte_array, QIODevice::WriteOnly);
+  std::shared_ptr<QByteArray> byte_array_ptr = std::make_shared<QByteArray>();
+  QDataStream ds(byte_array_ptr.get(), QIODevice::WriteOnly);
 
-  ds.writeRawData(hdr.sender_alias, 18);
+  ds.writeRawData(hdr->sender_alias, 18);
 
-  uint8_t s = hdr.options.size();
+  uint8_t s = hdr->options.size();
   ds << s;
 
-  for (const auto &opt : hdr.options)
+  for (const auto &opt : hdr->options)
   {
     ds << opt.type;
     ds << opt.len;
     ds.writeRawData(opt.data.get(), opt.len);
   }
 
-  return byte_array;
+  return byte_array_ptr;
 }
 
-HSTP_Header HstpHandler::_deserialize(const QByteArray &buff)
+std::shared_ptr<HSTP_Header>
+HstpHandler::_deserialize(const std::shared_ptr<QByteArray> &buff)
 {
-  HSTP_Header hdr;
-  QDataStream ds(buff);
+
+  std::shared_ptr<HSTP_Header> hdr = std::make_shared<HSTP_Header>();
+  QDataStream ds(buff.get(), QIODevice::ReadOnly);
 
   // get alias
-  ds.readRawData(hdr.sender_alias, 18);
+  ds.readRawData(hdr->sender_alias, 18);
 
   // get option count
   uint8_t opt_count;
@@ -120,7 +106,7 @@ HSTP_Header HstpHandler::_deserialize(const QByteArray &buff)
     opt.data = std::shared_ptr<char[]>(new char[opt.len]);
     ds.readRawData(opt.data.get(), opt.len);
 
-    hdr.options.push_back(opt);
+    hdr->options.push_back(opt);
   }
 
   return hdr;
--- netlib/test/hstp_test.cpp
+++ netlib/test/hstp_test.cpp
@@ -1,7 +1,9 @@
+#include <QtCore/qcontainerfwd.h>
 #include <QtCore/qstringview.h>
 #include <cstddef>
 #include <gtest/gtest.h>
 #include <hstp.hpp>
+#include <memory>
 
 class HstpHandlerTest : public testing::Test
 {
@@ -15,8 +17,13 @@ public:
 
   void TearDown() override {}
 
-  QByteArray _serialize(HSTP_Header &hdr) { return m_handler._serialize(hdr); }
-  HSTP_Header _deserialize(const QByteArray &buff)
+  std::shared_ptr<QByteArray>
+  _serialize(const std::shared_ptr<HSTP_Header> &hdr)
+  {
+    return m_handler._serialize(hdr);
+  }
+  std::shared_ptr<HSTP_Header>
+  _deserialize(const std::shared_ptr<QByteArray> &buff)
   {
     return m_handler._deserialize(buff);
   }
@@ -24,9 +31,10 @@ public:
 
 TEST_F(HstpHandlerTest, SerializeTest_Basic)
 {
+
   Option echo_option;
-  HSTP_Header header;
-  strcpy(header.sender_alias, "Alex");
+  std::shared_ptr<HSTP_Header> header = std::make_shared<HSTP_Header>();
+  std::strcpy(header.get()->sender_alias, "Alex");
   echo_option.type = 0;
   echo_option.len = 6;
   echo_option.data = std::shared_ptr<char[]>(new char[echo_option.len]);
@@ -34,12 +42,12 @@ TEST_F(HstpHandlerTest, SerializeTest_Basic)
   std::strcpy(echo_option.data.get(), hello);
   echo_option.data[6] = 0;
 
-  header.options.push_back(echo_option);
+  header->options.push_back(echo_option);
 
-  QByteArray byte_array;
-  byte_array = _serialize(header);
+  std::shared_ptr<QByteArray> byte_array_ptr;
+  byte_array_ptr = _serialize(header);
 
-  EXPECT_EQ(byte_array.toHex(':').toStdString(),
+  EXPECT_EQ(byte_array_ptr->toHex(':').toStdString(),
             std::string("41:6c:65:78:00:00:00:00:00:00:00:00:00:00:00:00:00:"
                         "00:01:00:06:68:65:6c:6c:6f:00"));
 }
@@ -48,20 +56,21 @@ TEST_F(HstpHandlerTest, SerializeTest_18CharAlias)
 {
   Option echo_option;
 
-  HSTP_Header header;
-  strcpy(header.sender_alias, "AAAAAAAAAAAAAAAAAA");
+  std::shared_ptr<HSTP_Header> header = std::make_shared<HSTP_Header>();
+
+  strcpy(header->sender_alias, "AAAAAAAAAAAAAAAAAA");
   echo_option.type = 0;
   echo_option.len = 6;
   echo_option.data = std::shared_ptr<char[]>(new char[echo_option.len]);
   const char *hello = "hello";
   std::memcpy(echo_option.data.get(), hello, 6);
 
-  header.options.push_back(echo_option);
+  header->options.push_back(echo_option);
 
-  QByteArray byte_array;
+  std::shared_ptr<QByteArray> byte_array = std::make_shared<QByteArray>();
   byte_array = _serialize(header);
 
-  EXPECT_STREQ(byte_array.toHex(':').toStdString().c_str(),
+  EXPECT_STREQ(byte_array->toHex(':').toStdString().c_str(),
                "41:41:41:41:41:41:41:41:41:41:41:41:41:41:41:41:41:41:01:00:06:"
                "68:65:6c:6c:6f:00");
 }
@@ -69,8 +78,9 @@ TEST_F(HstpHandlerTest, SerializeTest_18CharAlias)
 TEST_F(HstpHandlerTest, DeserializeTest_Basic)
 {
   Option echo_option1, echo_option2;
-  HSTP_Header header;
-  strcpy(header.sender_alias, "Alex");
+  std::shared_ptr<HSTP_Header> header = std::make_shared<HSTP_Header>();
+
+  strcpy(header->sender_alias, "Alex");
   echo_option1.type = 0;
   echo_option1.len = 6;
   echo_option1.data = std::shared_ptr<char[]>(new char[echo_option1.len]);
@@ -80,37 +90,40 @@ TEST_F(HstpHandlerTest, DeserializeTest_Basic)
   echo_option2.len = 6;
   echo_option2.data = std::shared_ptr<char[]>(new char[echo_option2.len]);
   std::memcpy(echo_option2.data.get(), "world", 6);
-  header.options.push_back(echo_option1);
-  header.options.push_back(echo_option2);
+  header->options.push_back(echo_option1);
+  header->options.push_back(echo_option2);
 
-  HSTP_Header deserialized_header = _deserialize(_serialize(header));
+  std::shared_ptr<HSTP_Header> deserialized_header =
+      _deserialize(_serialize(header));
 
-  EXPECT_EQ(header, deserialized_header);
+  EXPECT_EQ(*header, *deserialized_header);
 }
 
 TEST_F(HstpHandlerTest, DeserializeTest_BasicFailure)
 {
   Option echo_option1, echo_option2;
-  HSTP_Header header1;
-  strcpy(header1.sender_alias, "Alex");
+  std::shared_ptr<HSTP_Header> header1 = std::make_shared<HSTP_Header>();
+  strcpy(header1->sender_alias, "Alex");
   echo_option1.type = 0;
   echo_option1.len = 6;
   echo_option1.data = std::shared_ptr<char[]>(new char[echo_option1.len]);
   std::memcpy(echo_option1.data.get(), "hello", 6);
 
-  HSTP_Header header2;
-  strcpy(header2.sender_alias, "Alex");
+  std::shared_ptr<HSTP_Header> header2 = std::make_shared<HSTP_Header>();
+  strcpy(header2->sender_alias, "Alex");
   echo_option2.type = 0;
   echo_option2.len = 6;
   echo_option2.data = std::shared_ptr<char[]>(new char[echo_option2.len]);
   std::memcpy(echo_option2.data.get(), "world", 6);
-  header2.options.push_back(echo_option1);
-  header2.options.push_back(echo_option2);
+  header2->options.push_back(echo_option1);
+  header2->options.push_back(echo_option2);
 
-  HSTP_Header deserialized_header1 = _deserialize(_serialize(header1));
-  HSTP_Header deserialized_header2 = _deserialize(_serialize(header2));
+  std::shared_ptr<HSTP_Header> deserialized_header1 =
+      _deserialize(_serialize(header1));
+  std::shared_ptr<HSTP_Header> deserialized_header2 =
+      _deserialize(_serialize(header2));
 
-  EXPECT_EQ(header1, deserialized_header1);
-  EXPECT_EQ(header2, deserialized_header2);
-  EXPECT_NE(deserialized_header1, deserialized_header2);
+  EXPECT_EQ(*header1, *deserialized_header1);
+  EXPECT_EQ(*header2, *deserialized_header2);
+  EXPECT_NE(*deserialized_header1, *deserialized_header2);
 }

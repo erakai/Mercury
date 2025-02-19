9c17ab2f35624e9b2bae7dfb2be07781a1bb5c1f
Kai Tinkess <kaitinkess@gmail.com>
2025-02-19 00:04:39 -0500

    format.sh update to account for .h files

--- format.sh
+++ format.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Copy pre-commit hook if it doesn't exist
-cp -n pre-commit .git/hooks/pre-commit
+file=".git/hooks/pre-commit"
+if [ -f "$file" ] ; then
+    rm ${file}
+fi
+cp -n pre-commit ${file}
 
 # Format repository
-find netlib -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -Werror -i
-find app -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -Werror -i
+find netlib -iname '*.h*' -o -iname '*.cpp' | xargs clang-format -Werror -i
+find app -iname '*.h*' -o -iname '*.cpp' | xargs clang-format -Werror -i
 
 echo "Formatting repository."

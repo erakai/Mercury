869dbaa6a4af42b1443bbabb36f0a6a04c2e969b
Kris Leungwattanakij <kleungwa@purdue.edu>
2025-02-27 17:39:46 -0500

    [refactor] moved code for sidepane to .cpp file

--- app/stream/sidepane.cpp
+++ app/stream/sidepane.cpp
@@ -1,5 +1,34 @@
 #include "sidepane.hpp"
 
+SidePane::SidePane(QWidget *parent) : QWidget(parent)
+{
+  QPalette pal = QPalette();
+  pal.setColor(QPalette::Window, Qt::gray);
+  setAutoFillBackground(true);
+  setPalette(pal);
+
+  QVBoxLayout *layout = new QVBoxLayout(this);
+  chatBox = new QListWidget(this);
+
+  QPalette chatBoxPalette = chatBox->palette();
+  chatBoxPalette.setColor(QPalette::Base, Qt::white);
+  chatBoxPalette.setColor(QPalette::Text, Qt::black);
+  chatBox->setPalette(chatBoxPalette);
+
+  chatBox->setSelectionMode(QAbstractItemView::NoSelection);
+  chatBox->setFocusPolicy(Qt::NoFocus);
+  chatBox->setEditTriggers(QAbstractItemView::NoEditTriggers);
+  chatBox->setEnabled(false);
+
+  QString formattedMsg = QString::fromStdString("User 1:\nKai loves Alex\n");
+  chatBox->addItem(new QListWidgetItem(formattedMsg));
+  QString formattedMsg2 = QString::fromStdString("User 2:\nNo he doesn't\n");
+  chatBox->addItem(new QListWidgetItem(formattedMsg2));
+
+  layout->addWidget(chatBox);
+  setLayout(layout);
+}
+
 void SidePane::new_chat_message(ChatMessage msg)
 {
   // idk display it
--- app/stream/sidepane.hpp
+++ app/stream/sidepane.hpp
@@ -23,34 +23,7 @@ class SidePane : public QWidget
   Q_OBJECT;
 
 public:
-  SidePane(QWidget *parent) : QWidget(parent)
-  {
-    QPalette pal = QPalette();
-    pal.setColor(QPalette::Window, Qt::gray);
-    setAutoFillBackground(true);
-    setPalette(pal);
-
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    chatBox = new QListWidget(this);
-
-    QPalette chatBoxPalette = chatBox->palette();
-    chatBoxPalette.setColor(QPalette::Base, Qt::white);
-    chatBoxPalette.setColor(QPalette::Text, Qt::black);
-    chatBox->setPalette(chatBoxPalette);
-
-    chatBox->setSelectionMode(QAbstractItemView::NoSelection);
-    chatBox->setFocusPolicy(Qt::NoFocus);
-    chatBox->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    chatBox->setEnabled(false);
-
-    QString formattedMsg = QString::fromStdString("User 1:\nKai loves Alex\n");
-    chatBox->addItem(new QListWidgetItem(formattedMsg));
-    QString formattedMsg2 = QString::fromStdString("User 2:\nNo he doesn't\n");
-    chatBox->addItem(new QListWidgetItem(formattedMsg2));
-
-    layout->addWidget(chatBox);
-    setLayout(layout);
-  }
+  SidePane(QWidget *parent = nullptr);
 
   void new_chat_message(ChatMessage msg);
 

5c1142981becbe5eb66e64bd0d4575374e76bd72
Kris Leungwattanakij <kleungwa@purdue.edu>
2025-02-27 17:34:11 -0500

    [feat] added chatbox frame

--- app/stream/sidepane.cpp
+++ app/stream/sidepane.cpp
@@ -3,4 +3,7 @@
 void SidePane::new_chat_message(ChatMessage msg)
 {
   // idk display it
+  //TODO: render on front end
+  //msg.sender = username
+  //msg.message = message content
 }
--- app/stream/sidepane.hpp
+++ app/stream/sidepane.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <QListWidget>
+#include <QListWidgetItem>
 #include <QPalette>
+#include <QVBoxLayout>
 #include <QWidget>
 
 struct ChatMessage
@@ -23,9 +26,30 @@ public:
   SidePane(QWidget *parent) : QWidget(parent)
   {
     QPalette pal = QPalette();
-    pal.setColor(QPalette::Window, Qt::red);
+    pal.setColor(QPalette::Window, Qt::gray);
     setAutoFillBackground(true);
     setPalette(pal);
+
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    chatBox = new QListWidget(this);
+
+    QPalette chatBoxPalette = chatBox->palette();
+    chatBoxPalette.setColor(QPalette::Base, Qt::white);
+    chatBoxPalette.setColor(QPalette::Text, Qt::black);
+    chatBox->setPalette(chatBoxPalette);
+
+    chatBox->setSelectionMode(QAbstractItemView::NoSelection);
+    chatBox->setFocusPolicy(Qt::NoFocus);
+    chatBox->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    chatBox->setEnabled(false);
+
+    QString formattedMsg = QString::fromStdString("User 1:\nKai loves Alex\n");
+    chatBox->addItem(new QListWidgetItem(formattedMsg));
+    QString formattedMsg2 = QString::fromStdString("User 2:\nNo he doesn't\n");
+    chatBox->addItem(new QListWidgetItem(formattedMsg2));
+
+    layout->addWidget(chatBox);
+    setLayout(layout);
   }
 
   void new_chat_message(ChatMessage msg);
@@ -34,4 +58,5 @@ signals:
   void send_chat_message(std::string message);
 
 private:
+  QListWidget *chatBox;
 };
